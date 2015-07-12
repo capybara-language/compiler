0d2937c271f59b6eae4e7de56014e87fc2cb7c50
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-07-12 16:57:31 -0300

    Unit tests and folder organization

---
+++ test/TestLexer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace TestLexer
+{
+  require_once "../src/lexer/TokenList.php";
+  require_once "../src/lexer/Token.php";
+  require_once "../src/lexer/Assertion.php";
+  require_once "../src/lexer/Lexer.php";
+  require_once "../src/lexer/Tokenizer.php";
+
+  class TestLexer extends \PHPUnit_Framework_TestCase
+  {
+    protected $tokenStack = [];
+
+    public function __construct()
+    {
+      $source = file_get_contents("../examples/HelloWorld.capy");
+
+      $lexer = new \CapyLexer\Tokenizer($source);
+      $token = $lexer->nextToken();
+
+      while ($token->getKey() != \CapyLexer\Lexer::T_EOF) {
+        $this->tokenStack[] = $token;
+        $token = $lexer->nextToken();
+      }
+    }
+
+    public function testLength()
+    {
+      $this->assertEquals(130, sizeof($this->tokenStack));
+    }
+
+    public function testTokenOutput()
+    {
+      $this->expectf = explode("\n", file_get_contents("lexer_expect.phpt"));
+      array_pop($this->expectf);
+
+      $i = 0;
+      while ($i < sizeof($this->expectf)) {
+        $this->assertEquals($this->expectf[$i] . "\n",
+          (string) $this->tokenStack[$i]);
+        $i++;
+      }
+    }
+  }
+}
