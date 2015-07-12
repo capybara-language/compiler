1e0e7aa209fd5ebdf40e851b2dd1029e2a62def5
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-07-12 17:03:47 -0300

    Test folder

--- tests/TestLexer.php
+++ tests/TestLexer.php
@@ -2,11 +2,11 @@
 
 namespace TestLexer
 {
-  require_once "../src/lexer/TokenList.php";
-  require_once "../src/lexer/Token.php";
-  require_once "../src/lexer/Assertion.php";
-  require_once "../src/lexer/Lexer.php";
-  require_once "../src/lexer/Tokenizer.php";
+  require_once "./src/lexer/TokenList.php";
+  require_once "./src/lexer/Token.php";
+  require_once "./src/lexer/Assertion.php";
+  require_once "./src/lexer/Lexer.php";
+  require_once "./src/lexer/Tokenizer.php";
 
   class TestLexer extends \PHPUnit_Framework_TestCase
   {
@@ -14,7 +14,7 @@ namespace TestLexer
 
     public function __construct()
     {
-      $source = file_get_contents("../examples/HelloWorld.capy");
+      $source = file_get_contents("./examples/HelloWorld.capy");
 
       $lexer = new \CapyLexer\Tokenizer($source);
       $token = $lexer->nextToken();
@@ -32,7 +32,8 @@ namespace TestLexer
 
     public function testTokenOutput()
     {
-      $this->expectf = explode("\n", file_get_contents("lexer_expect.phpt"));
+      $this->expectf = explode("\n",
+        file_get_contents("./tests/lexer_expect.phpt"));
       array_pop($this->expectf);
 
       $i = 0;
